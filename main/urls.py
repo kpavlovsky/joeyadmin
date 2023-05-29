626eb405eb4295072f6cafb5b821576d1a1fa242
Kostja P <kpavlovsky@users.noreply.github.com>
2023-05-29 15:45:36 +0200

    workorder improvement and inline edit

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from .api.user import UserInfoAPIView
-
+from .views import WorkOrderList
 urlpatterns = [
+    path('workorders/', WorkOrderList.as_view(), name='workorders'),
     path('user/info', UserInfoAPIView.as_view(), name='user_info'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,3 +1,13 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
+from django.views.generic import ListView
+
 
 # Create your views here.
+
+class WorkOrderList(LoginRequiredMixin, ListView):
+    template_name = 'main/workorder_list.html'
+    context_object_name = 'workorders'
+
+    def get_queryset(self):
+        return self.request.user.workorder_set.all()
