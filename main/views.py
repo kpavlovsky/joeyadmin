4c5a0d5632d21266078e1b85fefe4bdf95c2cbd2
Kostja P <kpavlovsky@users.noreply.github.com>
2023-05-29 17:09:23 +0200

    workorder improvement and inline edit

--- main/views.py
+++ main/views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
 from django.views.generic import ListView, CreateView
+from . import models
 
 
 # Create your views here.
@@ -15,5 +16,6 @@ class WorkOrderList(LoginRequiredMixin, ListView):
 
 class WorkOrderCreateView(LoginRequiredMixin, CreateView):
     template_name = 'main/workorder_create.html'
+    model = models.WorkOrder
     fields = ['title', 'scope', 'client', 'site', 'status']
     success_url = '/workorders/'
