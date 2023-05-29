b76f09437c3cdd7b886a13f1e0e384499ae74eb2
Kostja P <kpavlovsky@users.noreply.github.com>
2023-05-29 17:06:01 +0200

    workorder improvement and inline edit

--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from .api.user import UserInfoAPIView
-from .views import WorkOrderList
+from .views import WorkOrderList, WorkOrderCreateView
 urlpatterns = [
+    path('workorders/create', WorkOrderCreateView.as_view(), name='workorders_create'),
     path('workorders/', WorkOrderList.as_view(), name='workorders'),
     path('user/info', UserInfoAPIView.as_view(), name='user_info'),
 ]
